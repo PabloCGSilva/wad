1e7a13caf57cd54867f25f6230c66b36a2a440dc
Pablo Silva <pablocarlos1982@gmail.com>
2020-11-16 21:15:18 -0300

    module.exports

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const consumer_key = 'EbHZyzFdKfNdwRhQRV5iXy8rl';
+
 export const consumer_secret = 'eeuctI3edOxfVGYhhjvxBhAklx4tsJEAYjdyUSO1xJxV7AsMXi';
 export const access_token_key = '2534481467-HZpJsfpwvd8WXydEcL0pUZtu9lSHoJoUGvCfMoc';
 export const access_token_secret = 'xOLfcMbaMQsTiZ12NXWiHOkXfm5iqCNBXrAU65nyS4bWX';
