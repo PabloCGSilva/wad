10fd59a009a1be5147b6a5befd89df58e48b733f
Pablo Silva <pablocarlos1982@gmail.com>
2020-11-17 08:37:19 -0300

    back

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-
-export const consumer_secret = 'eeuctI3edOxfVGYhhjvxBhAklx4tsJEAYjdyUSO1xJxV7AsMXi';
-export const access_token_key = '2534481467-HZpJsfpwvd8WXydEcL0pUZtu9lSHoJoUGvCfMoc';
-export const access_token_secret = 'xOLfcMbaMQsTiZ12NXWiHOkXfm5iqCNBXrAU65nyS4bWX';
+module.exports = consumer_key = 'lM4prPOeDAsfjZwqWODSC19XC';
+module.exports = consumer_secret = 'jZrB39bSYlflPzEAPEssEvZljmXDJPNxEsGLApVqVMFnJhu8Lc';
+module.exports = access_token_key = '2534481467-4lkFm9OJo0biQ38Tht58cRNuJKY7VsfoO1FRtR8';
+module.exports = access_token_secret = 'ZF3cp46LOP2mpZN7H7P7y8p800GHwbYuWLqCxww2dGIhU';
