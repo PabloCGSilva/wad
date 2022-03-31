f88e900c8bdfb48c85ecac18468d8554fd480849
PabloCGSilva <pablocarlos1982@gmail.com>
2022-03-31 18:24:52 -0300

    apis

--- config.cjs
+++ config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-consumer_key            : 'ilh3SA6FhVUsk9NgBWO14vVcC',
-consumer_secret         : '20qwZQqiCYyAF8Jv4N4rmvUpwV59hX5FCQwLv5MyIAZkvuiK0u',
-access_token            : '2534481467-mi0UGSF2E9dCgftK0wuAlrKUhKgQe1CbT8VsYrM',
-access_token_secret     : 'RK0oIbMvOrzo63xf9d9dLocnjXnPmB6JEZPRrpdjCuxcJ'
+    consumer_key: 'ilh3SA6FhVUsk9NgBWO14vVcC',
+    consumer_secret: '20qwZQqiCYyAF8Jv4N4rmvUpwV59hX5FCQwLv5MyIAZkvuiK0u',
+    access_token: '2534481467-mi0UGSF2E9dCgftK0wuAlrKUhKgQe1CbT8VsYrM',
+    access_token_secret: 'RK0oIbMvOrzo63xf9d9dLocnjXnPmB6JEZPRrpdjCuxcJ',
+    API_KEY: "AIzaSyCrpAF9lG4Isc-Gnnzx76JbbADJS5estfE",
+    API_KEY_2: "KNcuyTpIb5us9ZvJcJrtfwT7SMhFiyJk",
 }
